6f491c47d324213d7caab22dad0b68db1142ecb1
dhianapereira <dhianaslv@outlook.com>
2024-01-12 23:30:31 -0300

    feat: Create Button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Plus } from 'phosphor-react';
+import { Button, ButtonProps } from '@campusativo-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    disabled: false,
+    children: "Enviar"
+  },
+  argTypes: {
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    }
+  }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+  }
+}
+
+export const IconButton: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        <Plus weight='bold' />
+        Add
+      </>
+    ),
+  }
+}
+
+
+export const IconOutlinedButton: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: (
+      <>
+        <Plus weight='bold' />
+        Add
+      </>
+    ),
+  }
+}
